f389755832b1dde20c7b780f64e21ff01584df29
Jonas Reich <mail@jonasreich.de>
2022-12-04 01:03:09 +0100

    Added support for mastodon post redirection

--- Mastodon/mastodon_go_home.user.js
+++ Mastodon/mastodon_go_home.user.js
@@ -11,7 +11,9 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        GM_addStyle
-// @grant        GM.getValue
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
 // ==/UserScript==
 
 // Based on user script by DiThi in https://github.com/rugk/mastodon-simplified-federation/issues/77#issuecomment-1328265632
@@ -20,6 +22,8 @@
 
 // PUT YOUR INSTANCE BELOW
 const INSTANCE = 'mastodon.gamedev.place';
+const MASTO_REMOTE_POST_URL_KEY = "mastodon-remote-url";
+const MASTO_REMOTE_USER_ID_KEY = "masto-user-url";
 
 function gotoMyInstance(){
     console.debug(location.href);
@@ -28,24 +32,46 @@ function gotoMyInstance(){
     if(parts[3].indexOf('@',1) === -1) {
         parts[3] += '@'+parts[2];
     }
+    let remote_user_id = parts[3];
     parts[2] = INSTANCE;
     if (parts.length >= 5) {
-        // URL is to a post
-        // not possible to redirect atm
-        let text = parts.join('/');
-        if (confirm("Cannot redirect to remote posts. You will be redirected to your instance's search instead. Please paste the post URL in to search (copied to your clipboard if you click ok).")) {
-            navigator.clipboard.writeText(location.href).then(function() {
-                console.log('Async: Copying to clipboard was successful !' + text);
-                parts[3] = "search";
-                location.href = parts.splice(0,4).join('/');
-            }, function(err) {
-                console.error('Async: Could not copy text: ' + text, err);
-            });
-        }
+        // This is a post. Need to store some extra info, so we can redirect to proper ID once we are in our home instance.
+        // We can't do this from the remote instance, because it can't access home instance API.
+        GM_setValue(MASTO_REMOTE_POST_URL_KEY, location.href);
+        GM_setValue(MASTO_REMOTE_USER_ID_KEY, remote_user_id);
+        // replace the post ID with something that makes it a bit more clear that this is not the final URL
+        parts[4] = "wait-for-userscript-redirect";
     } else {
         // URL is to a user profile/etc
         // redirect immediately
-        location.href = parts.join('/');
+    }
+    location.href = parts.join('/');
+}
+
+function openRemoteUrlInHomeInstance(remote_url, remote_user_id){
+    if (remote_url === undefined) {
+        return;
+    }
+
+    // If we were redirected from a remote instance, search for the local copy of that post.
+    // When found, redirect to the local copy.
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", `https://${INSTANCE}/api/v2/search?q=${encodeURI(remote_url)}&resolve=true`, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                var obj = JSON.parse(this.responseText);
+                // posts are called "statuses" in mastodon API
+                let statuses = obj.statuses;
+                if (statuses.length > 0)
+                {
+                    let local_id = statuses[0].id;
+                    location.href = `https://${INSTANCE}/${remote_user_id}/${local_id}`;
+                    return;
+                }
+            }
+        }
     }
 }
 
@@ -102,7 +128,14 @@ docReady(function() {
         return;
     }
     if (location.href.split('/').splice(0,4).join('/').includes(INSTANCE)) {
-        // Do not enable on home-instance
+        let remote_url = GM_getValue(MASTO_REMOTE_POST_URL_KEY);
+        GM_deleteValue(MASTO_REMOTE_POST_URL_KEY);
+        let user_id = GM_getValue(MASTO_REMOTE_USER_ID_KEY);
+        GM_deleteValue(MASTO_REMOTE_USER_ID_KEY);
+
+        openRemoteUrlInHomeInstance(remote_url, user_id);
+
+        // Do not enable other features on home-instance
         return;
     }
 
