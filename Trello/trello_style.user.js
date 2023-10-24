7ea877f97c3ea4fe1ed6764988553bf7613b96f9
Jonas Reich <mail@jonasreich.de>
2023-10-24 15:15:06 +0200

    Update trello_style.user.js

--- Trello/trello_style.user.js
+++ Trello/trello_style.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
-// @name          Trello Style (Jonas Reich)
+// @name          Trello Style (Grimlore)
 // @namespace     https://github.com/JonasReich/
-// @version       0.3.2
-// @description   Style Adjustments for Trello
+// @version       0.4.0
+// @description   Style Adjustments for Trello (for work at Grimlore)
 // @author        Jonas Reich
 // @match         https://trello.com/*
 // @icon          https://www.google.com/s2/favicons?sz=64&domain=trello.com
 // @updateURL     https://github.com/JonasReich/UserScripts/raw/main/Trello/trello_style.user.js
 // @downloadURL   https://github.com/JonasReich/UserScripts/raw/main/Trello/trello_style.user.js
+// @require       https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js
 // @grant         GM_addStyle
+// @grant         GM_setValue
+// @grant         GM_getValue
 // ==/UserScript==
 
 function replaceSeparatorsTick()
@@ -18,12 +21,12 @@ function replaceSeparatorsTick()
     let separator_string = "---";
     let dividers_card_titles = $(`.list-card-title:contains('${separator_string}')`);
     let cards = dividers_card_titles.closest(".list-card");
-    $(cards).find(".list-card-title:contains('done')").css("--card-separator-color", "#55b755"); // some shade of green
-    $(cards).find(".list-card-title:contains('todo')").css("--card-separator-color", "grey");
-    $(cards).find(".list-card-title:contains('started')").css("--card-separator-color", "#5050f7"); // some shade of blue
-    $(cards).find(".list-card-title:contains('in progress')").css("--card-separator-color", "#5050f7"); // some shade of blue
+    $(cards).find(".list-card-title:contains('done')").css("--card-separator-color", "#55b755").closest(".list-card").addClass("js-card-separator-done"); // some shade of green
+    $(cards).find(".list-card-title:contains('todo')").css("--card-separator-color", "grey").closest(".list-card").addClass("js-card-separator-todo");
+    $(cards).find(".list-card-title:contains('started')").css("--card-separator-color", "#5050f7").closest(".list-card").addClass("js-card-separator-wip"); // some shade of blue
+    $(cards).find(".list-card-title:contains('in progress')").css("--card-separator-color", "#5050f7").closest(".list-card").addClass("js-card-separator-wip"); // some shade of blue
 
-    dividers_card_titles.closest(".list-card").addClass("card-separator");
+    dividers_card_titles.closest(".list-card").addClass("js-card-separator");
     dividers_card_titles.each(function() {
         var str = $(this).contents().filter(function() {
             // only the immediate text. There are some hidden text elements (invisible card ID text) that we need to ignore.
@@ -31,6 +34,38 @@ function replaceSeparatorsTick()
         }).text().replace(separator_string, "");
         $(this).text(str);
     });
+
+    // mark done cards
+    $(".js-done-card").removeClass("js-done-card");
+    $(".js-card-separator-done").nextUntil(".js-card-separator-wip").addClass("js-done-card");
+
+    // mark wip cards
+    $(".js-wip-card").removeClass("js-wip-card");
+    $(".js-card-separator-wip").nextUntil(".js-card-separator-todo").addClass("js-wip-card");
+
+    // mark todo cards
+    $(".js-todo-card").removeClass("js-todo-card");
+    $(".js-card-separator-todo").nextAll().addClass("js-todo-card");
+}
+
+function updateDueDateButtonLabel() {
+    let show = GM_getValue(KEY_SHOW_DUES);
+    let btn_text = (show == "none") ? "🙈 Due Dates" : "👀 Due Dates";
+    $("#toggle-due-visibility-button-label").text(btn_text);
+}
+
+var KEY_SHOW_DUES = "trello-show-due-dates";
+function toggleDueDateVisibility()
+{
+    let display_status = $(":root").css("--js-display-due-badge");
+    if (display_status == "none") {
+        display_status = "inline-block";
+    } else {
+        display_status = "none";
+    }
+    GM_setValue(KEY_SHOW_DUES, display_status);
+    $(":root").css("--js-display-due-badge", display_status);
+    updateDueDateButtonLabel();
 }
 
 (function() {
@@ -77,14 +112,14 @@ function replaceSeparatorsTick()
         --card-separator-color: black;
     }
 
-    .card-separator {
+    .js-card-separator {
         min-height: 35px;
         border-radius: 0px;
         background-color: transparent !important;
         box-shadow: none;
     }
 
-    .card-separator .list-card-title {
+    .js-card-separator .list-card-title {
         color: var(--card-separator-color) !important;
         font-weight: 600;
         font-size: 1.1rem;
@@ -93,11 +128,11 @@ function replaceSeparatorsTick()
         padding-bottom: 10px;
     }
 
-    .card-separator .list-card-details {
+    .js-card-separator .list-card-details {
         padding: 0;
     }
 
-    .card-separator .list-card-title::before, .card-separator .list-card-title::after {
+    .js-card-separator .list-card-title::before, .js-card-separator .list-card-title::after {
         content: "";
         flex-grow: 1;
         display: inline;
@@ -107,10 +142,24 @@ function replaceSeparatorsTick()
         height: 12px;
     }
 
-    .card-separator .list-card-title.js-card-name {
+    .js-card-separator .list-card-title.js-card-name {
         display: flex;
     }
     `);
+
+    // done/wip/todo cards
+     GM_addStyle(`
+     .js-done-card {
+         opacity: 50%;
+     }
+     .js-done-card .badges, .js-done-card .list-card-members {
+         display: none;
+     }
+
+     .js-todo-card {
+         opacity: 80%;
+     }
+     `);
 
     // Hide strelloids card counter no matter the setting (it's a bit buggy)
     // Reccommendation: Use column limits instead.
@@ -151,5 +200,33 @@ function replaceSeparatorsTick()
     }
     `);
 
+    GM_addStyle(`
+    :root {
+        --js-display-due-badge: inline-block;
+    }
+    .js-due-date-badge {
+        display: var(--js-display-due-badge) !important;
+    }
+    `);
+
     setInterval(replaceSeparatorsTick, 100);
+
+    var added_button = false;
+    waitForKeyElements("div.board-header-btns", function(){
+        if (!added_button)
+        {
+            let button_template = `<div><div><div role="presentation"><button id="toggle-due-visibility-button" type="button" aria-label="Toggle Due Date Visibility" aria-describedby="36val-tooltip"><span id="toggle-due-visibility-button-label">Toggle Due Dates</span></button></div></div></div>`;
+            $(".board-header-btns").append(button_template);
+            $("#toggle-due-visibility-button").click(function(){toggleDueDateVisibility();});
+            updateDueDateButtonLabel();
+        }
+        added_button = true;
+    });
+
+    let due_visibility = GM_getValue(KEY_SHOW_DUES);
+    if (due_visibility != undefined) {
+        $(":root").css("--js-display-due-badge", due_visibility);
+    } else {
+        GM_setValue("inline-block");
+    }
 })();
